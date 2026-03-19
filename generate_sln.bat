2ec06740595e617430701ca0361736033cbcfca5
Prince Nindrei G. Pamintuan <princei.cs50@gmail.com>
2026-03-19 19:44:54 +0800

    Added support for VS2019 and VS2022 by picking which version

--- generate_sln.bat
+++ generate_sln.bat
@@ -1,8 +1,24 @@
 @echo off
-echo Generating Visual Studio 2026 solution using Premake...
+echo Select Visual Studio version:
+echo 1. VS 2019
+echo 2. VS 2022
+echo 3. VS 2026
+set /p choice=Enter choice (1-3): 
 
-rem -- generate VS 2026 solution
-premake5 vs2026
+if "%choice%"=="1" (
+    echo Generating VS 2019 solution...
+    premake5 vs2019
+) else if "%choice%"=="2" (
+    echo Generating VS 2022 solution...
+    premake5 vs2022
+) else if "%choice%"=="3" (
+    echo Generating VS 2026 solution...
+    premake5 vs2026
+) else (
+    echo Invalid choice!
+    pause
+    exit /b
+)
 
 echo Done!
 echo Open Opac.sln
